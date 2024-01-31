994a8f51ec22cee2eaad94b136daec98f84fba74
Eugene Dovganich <passionuberalles@yandex.ru>
2024-01-31 17:00:36 +0300

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,16 @@
+from random import randint
+
+number = randint(1, 100)
+print("Guess the number between 1 and 100")
+
+while True:
+    guess = int(input("Enter your number: "))
+
+    if guess > number:
+        print("Your number is more")
+    elif guess < number:
+        print("Your number is less")
+    elif guess == number:
+        break
+
+print("Good job!")
